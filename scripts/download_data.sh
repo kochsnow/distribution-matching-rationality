0719888b4aaa1f37374e66beb4f245b7cd069f2f
kochsnow <huangyf17@mails.tsinghua.edu.cn>
2021-01-16 21:17:18 +0800

    Update download_data.sh

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -5,9 +5,22 @@ then
 else
     mkdir "data"
     cd data
-    wget http://people.csail.mit.edu/chang87/files/beer_review.zip
-    unzip beer_review.zip      
-    cd ..    
+    wget https://people.csail.mit.edu/yujia/files/r2a/data.zip
+    unzip data.zip      
+    mkdir hotel_review
+    cd hotel_review
+    mkdir hotel0 hotel1 hotel2 annoated
+    #text data
+    cp ../data/Orcale/hotel_Location.train hotel0/train.tsv
+    cp ../data/Orcale/hotel_Location.dev hotel0/dev.tsv
+    cp ../data/Orcale/hotel_Service.train hotel1/train.tsv
+    cp ../data/Orcale/hotel_Service.dev hotel1/dev.tsv
+    cp ../data/Orcale/hotel_Cleanliness.train hotel2/train.tsv
+    cp ../data/Orcale/hotel_Cleanliness.dev hotel2/dev.tsv
+    #annoated data
+    cp  ../data/target/hotel_Cleanliness.train annoated/hotel_Cleanliness.train
+    cp ../data/target/hotel_Location.train annoated/hotel_Location.train 
+    cp ../data/target/hotel_Service.train annoated/hotel_Service.train 
 fi
 
 # Get the embedding
